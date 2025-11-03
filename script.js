ec416bdb6dd9156979af394d5963b6383e86f46a
szabobotondckik <szabo.botond@ckik.hu>
2025-11-03 12:29:35 +0100

    Elemeket már lehet felvenni és css elkezdése

--- script.js
+++ script.js
@@ -10,5 +10,24 @@ function add() {
 
     list.push(textarea.value)
     console.log(list);
+    for (let index = 0; index < list.length; index++) {
+        localStorage.setItem(String(index), list[index])
+        console.log(localStorage.getItem(String(index), list[index]));
+        
+    }
+    
+    
+    
     ul.appendChild(document.createElement("li")).innerText = textarea.value;
+    const li = document.querySelector("li");
+    textarea.value = "";
+    // show()
 }
+
+// function  show() {
+//     for (let index = 0; index < list.length; index++) {
+        
+//         ul.appendChild(document.createElement("li")).innerText = localStorage.index;
+        
+//     }
+// }
