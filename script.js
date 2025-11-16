f3bdb4f12120b2a5d98ad4d4d5af2096436058a5
toth.tamas <toth.tamas@ckik.hu>
2025-11-16 18:03:22 +0100

    checked + delete javítása (még nem sikerült)

--- script.js
+++ script.js
@@ -11,26 +11,21 @@ window.addEventListener("load", show);
 
 function add() {
     if (textarea.value === "") return;
-    // const data   = {
-    //     text,
-    //     checked
-    // };
-    
-    // data.text = textarea.value;
-    // data.checked = "false";
+    if (localStorage.length > 0) {
+        for (let index = 0; index < localStorage.length; index++) {
+            list[index] = localStorage.getItem(String(index))
+        }
+    }
     list.push(textarea.value)
-    console.log(list);
+
+
     for (let index = 0; index < list.length; index++) {
         localStorage.setItem(String(index), list[index])
-        // console.log(localStorage.getItem(String(index), list[index]));
-        
+        console.log(localStorage.getItem(String(index), list[index]));
     }
-    
-    const li = document.createElement("li");
-    li.innerText = textarea.value;
-    ul.appendChild(li);
+
     textarea.value = "";
-    // show()
+    show()
     
 }
 
@@ -38,12 +33,11 @@ function add() {
 function show() {
     ul.innerHTML = "";
     
-    for (let i = 0; i < localStorage.length; i++) {
-        const value = localStorage.getItem(String(i));
+    for (let index = 0; index < localStorage.length; index++) {
+        const value = localStorage.getItem(String(index));
         if (value !== null && value !== "") {
-            list.push(value);
             const div = document.createElement("div");
-            div.dataset.id = String(i);
+            div.dataset.id = String(index);
             const li = document.createElement("li")
             const deletebtn = document.createElement("button")
             deletebtn.innerText = "Törlés"
@@ -56,33 +50,32 @@ function show() {
             div.appendChild(checkBox)
             div.appendChild(li);
             div.appendChild(deletebtn)
-            
-            
             ul.appendChild(div);
+            
             deletebtn.addEventListener("click", deleteData);
+            checkBox.addEventListener("click", check)
         }
     }
-
-    
 }
 
-
+function check(event){
+    const i = event.target.dataset.id;
+    const done = event.target;
+    console.log(done);
+    if (done.checked) {
+        done.parentElement.style.backgroundColor = "green";
+    }
+    else {
+        done.parentElement.style.backgroundColor = "bisque";
+    }
+}
 
 function deleteData(event) {
     
     const i = event.target.parentElement.dataset.id;
-
-    // localStorage.removeItem();
-    // ul.remove();
-    
-    // console.log(event.target.parentElement.dataset.id);
-    // localStorage.removeItem()
-    // delete(event.target.parentElement);
     ul.removeChild(event.target.parentElement)
     localStorage.removeItem(String(i));
-    // document.removeChild();
-
-    
+    // list.remove(i);
 }
 
 
